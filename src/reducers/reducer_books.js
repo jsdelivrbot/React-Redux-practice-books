2313bd83cffa1a6ec05af9ca03e29dcd8ff64ff4
Andres Saldarriaga <jandreus87@hotmail.com>
2018-01-19 14:42:15 -0500

    Complete

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return[
-    { title: 'Javascript: The good parts'},
-    { title: 'Harry Potter'},
-    { title: 'Mandala'},
-    { title: 'Converstios with God'}
+    { title: 'Javascript: The good parts', pages: 500},
+    { title: 'Harry Potter', pages: 456},
+    { title: 'Mandala', pages: 120},
+    { title: 'Converstios with God', pages: 370}
   ]
 }
